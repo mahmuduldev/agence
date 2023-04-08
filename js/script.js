83ca383169cfc59d83c03d72d3459c4862c1ff18
mahmuduldev <hassanmahmudul917@gmail.com>
2023-04-08 18:20:30 +0600

    code correction

--- js/script.js
+++ js/script.js
@@ -22,6 +22,31 @@ $(function(){
         nextArrow: ".icon-right",
         centerMode: true,
         centerPadding: '0',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 })
 
